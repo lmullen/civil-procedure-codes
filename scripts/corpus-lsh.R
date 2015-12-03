0d3e297e5ee8835ec08b489697c4dae9ce44d417
Lincoln Mullen <lincoln@lincolnmullen.com>
2015-12-02 20:11:20 -0500

    Cache munging of data to section to section matches

--- scripts/corpus-lsh.R
+++ scripts/corpus-lsh.R
@@ -1,8 +1,7 @@
 library("textreuse")
 library("dplyr")
 library("stringr")
-source("R/extract_code_names.R")
-source("R/extract-date.R")
+source("R/helper.R")
 options("mc.cores" = 6L)
 
 h <- 120
@@ -15,12 +14,27 @@ sections <- TextReuseCorpus(dir = "legal-codes-split",
                             minhash_func = minhash)
 
 buckets <- lsh(sections, bands = b)
+
 scores <- buckets %>%
   lsh_candidates() %>%
-  lsh_compare(sections, jaccard_similarity) %>%
-  rename(section_a = a, section_b = b, similarity = score) %>%
-  mutate(code_a = extract_code_names(section_a),
-         code_b = extract_code_names(section_b),
-         year_a = extract_date(code_a),
-         year_b = extract_date(code_b))
-save(sections, buckets, scores, file = "cache/corpus-lsh.rda")
+  lsh_compare(sections, jaccard_similarity)
+
+scores_swapped <- scores %>%
+  rename(b2 = a, a2 = b) %>%
+  rename(a = a2, b = b2)
+
+scores_for_join <- bind_rows(scores, scores_swapped) %>%
+  rename(borrower_section = a,
+         match_section = b)
+
+matches <- data_frame(borrower_section = names(sections)) %>%
+  left_join(scores_for_join, by = "borrower_section") %>%
+  mutate(borrower_code = extract_code_names(borrower_section),
+         borrower_year = extract_date(borrower_section),
+         borrower_state = extract_state(borrower_section),
+         match_code = extract_code_names(match_section),
+         match_year = extract_date(match_section),
+         match_state = extract_state(match_section)) %>%
+  arrange(borrower_section, match_section)
+
+save(sections, buckets, scores, matches, file = "cache/corpus-lsh.rda")
