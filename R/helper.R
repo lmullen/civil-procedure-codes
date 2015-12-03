ef6d8c96ce6d764302284f08d7c7d88ebe7b5111
Lincoln Mullen <lincoln@lincolnmullen.com>
2015-12-02 21:03:25 -0500

    Pretty number helper

--- R/helper.R
+++ R/helper.R
@@ -16,3 +16,5 @@ extract_code_names <- function(section_names) {
   splits <- str_split(section_names, "-")
   vapply(splits, `[`, character(1), 1)
 }
+
+pnum <- function(x) prettyNum(x, big.mark = ",")
