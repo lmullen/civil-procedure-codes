c46dfbe77c1d4ca43dbe13ba710159b987aaa7d3
Lincoln Mullen <lincoln@lincolnmullen.com>
2016-01-05 12:29:33 -0500

    Working version of spectrogram function

--- R/spectrogram.R
+++ R/spectrogram.R
@@ -1,31 +1,49 @@
-
-# Threshold is set to 0 because in `get_best_matches()` we already applied a
-# threshold of `0.1`: in other words, if it didn't meet that minimum, we don't
-# consider it a match at all. Here we will have the option to increase the limit
-# but won't exercise it by default.
-#
 # The `white_list` argument takes either a count numeric, in which case it
 # white lists at most that number of codes to be explicitly named in the viz,
 # or a character vector of codes to white list.
-spectrogram <- function(code, matches_df, num_cols = 40, threshold = 0.0,
-                        white_list = NULL) {
+spectrogram <- function(code, matches_df, num_cols = 40, white_list = NULL) {
   require(dplyr)
+  require(stringr)
+  require(ggplot2)
 
   # Get just the genuine matches for the code of interest
   matches_df <- matches_df %>%
-    filter(borrower_code == code) %>%
-    filter(score >= threshold)
+    filter(borrower_code == code)
 
   # Create the white list of codes that we want to explicitly name in the viz.
   if (is.null(white_list)) {
     white_list <- white_list_maker(matches_df)
   } else if (is.numeric(white_list)) {
-    white_list <- white_list_maker(matches_df, n = white_list)
+    white_list <- white_list_maker(matches_df, num_codes = white_list)
   }
 
   # Make all other codes "other"
-  match_df <- match_df %>%
-    mutate(match_code = other_maker(code_code, white_list))
+  matches_df <- matches_df %>%
+    mutate(match_code = other_maker(match_code, white_list))
+
+  # Assign the positions in the heatmap
+  matches_df <- matches_df %>%
+    mutate(.,
+           column = col_vector(nrow(.), num_cols),
+           row = row_vector(nrow(.), num_cols),
+           match_code = as.factor(match_code))
+
+  # Create the title
+  title <- str_c("Borrowed sections in ", code)
+
+  # Make the plot
+  matches_df %>%
+  ggplot(aes(x = column, y = -row, fill = match_code)) +
+  geom_tile(color = "lightgray") +
+  theme_minimal(base_size = 16) +
+  coord_equal() +
+  labs(x = NULL, y = NULL, title = title,
+       fill = "Section borrowed from") +
+  theme(legend.position = "bottom",
+        axis.ticks = element_blank(),
+        axis.line = element_blank(), axis.text = element_blank(),
+        panel.background = element_blank(), panel.grid = element_blank()) +
+  scale_fill_brewer(type = "qual", na.value = "gray", palette = 6)
 
 }
 
@@ -50,31 +68,11 @@ other_maker <- function(x, white_list) {
   ifelse(is.na(x), NA, ifelse(x %in% white_list, x, "Other"))
 }
 
-white_list_maker <- function(df, n = 5) {
+white_list_maker <- function(df, num_codes = 5) {
   df %>%
     count(match_code) %>%
-    top_n(n, n) %>%
+    top_n(num_codes) %>%
     arrange(desc(n)) %>%
     filter(!is.na(match_code)) %>%
     `$`("match_code")
 }
-
-
-best_section_matches("WA1855", scores, threshold = 0.3) %>%
-  mutate(match_code = other_maker(match_code, white_list)) %>%
-  mutate(.,
-         column = col_vector(nrow(.), n_columns),
-         row = row_vector(nrow(.), n_columns),
-         match_code = as.factor(match_code)) %>%
-  ggplot(aes(x = column, y = -row, fill = match_code)) +
-  geom_tile(color = "lightgray") +
-  theme_minimal(base_size = 16) +
-  coord_equal() +
-  labs(x = NULL, y = NULL, title = "Borrowed sections in WA1855",
-       fill = "Section borrowed from") +
-  theme(legend.position = "bottom",
-        axis.ticks = element_blank(),
-        axis.line = element_blank(), axis.text = element_blank(),
-        panel.background = element_blank(), panel.grid = element_blank()) +
-  scale_fill_brewer(type = "qual", na.value = "gray", palette = 6)
-
